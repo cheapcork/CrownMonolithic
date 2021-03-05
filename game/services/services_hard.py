38163dab1abe360ef6cef9ef9f63219863d5f441
BeepBoopYeetYourScoot <zdmitry@alabuga.ru>
2021-03-05 16:44:06 +0300

    Added README.md. Separated services into normal and hard version

---
+++ game/services/services_hard.py
@@ -0,0 +1,8 @@
+"""
+Расчёт игровых параметров на начало хода версии Hard.
+Данный алгоритм импортирует классы Короны, маклера, производителя, и производит расчёт согласно утверждённой логике.
+"""
+from .crown import Crown
+from .manufacturer import Manufacturer
+from .broker import Broker
+
