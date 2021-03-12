b352fe484873968e098041748565f5b93eaca809
BeepBoopYeetYourScoot <zdmitry@alabuga.ru>
2021-03-12 19:10:12 +0300

    Разбил маклеров на нормал и хард

--- game/services/broker.py
+++ game/services/broker.py
@@ -1,86 +1,116 @@
-class Broker:
-
-    # TODO Можно поколдовать над методом и сделать зависимость от режима игры
-    def __init__(self, balance, is_bankrupt=False):
-        self.balance = balance
-        self.shipments_spruce = []
-        self.shipments_oak = []
-        self.shipments_redwood = []
-        self.is_bankrupt = is_bankrupt
-        self.shipments_normal = []
-        # В транзакции будут записываться только одобренные маклером сделки, которые проходят все возможные проверки
-        self.transactions = []
+class AbstractBroker:
 
     fixed_costs = 1000
-    # FIXME Сейчас расчёт затрат и выручки по материалам идёт отдельными методами. Можно либо оставить
-    #  методы, как есть, а можно объединить.
-    #  Объединить их можно, если сделать склад для харда в виде кортежа из 3х списков
+
+    material = {
+        'spruce': 0.5,
+        'oak': 0.7,
+        'redwood': 1
+    }
+
+    machine_quality = {
+        'chinese': 0.5,
+        'korean': 0.6,
+        'german': 1
+    }
 
     def add_shipments(self) -> None:
-        """Отправляет на склад маклера заготовки согласно их материалу"""
-        for transaction in self.transactions:
-            shipment = transaction['terms']['billets']
-            if shipment[1] == 0.5:
-                self.shipments_spruce.append(shipment)
-                return
-            elif shipment[1] == 0.7:
-                self.shipments_oak.append(shipment)
-                return
-            elif shipment[1] == 1:
-                self.shipments_redwood.append(shipment)
-                return
-            elif shipment[1] == 'normal':
-                self.shipments_normal.append(shipment)
-                return
-            raise AttributeError
+        """Отправляет заготовки на продажу Короне"""
+        pass
+
+    def make_deal(self, deal: dict) -> None:
+        """Совершает сделку с производителем"""
+        pass
+
+
+class BrokerNormal(AbstractBroker):
+    def __init__(self, balance):
+        self.balance = balance
+        self.shipment = 0
+        self.transactions = []
+
+    def add_shipments(self) -> None:
+        if self.transactions:
+            for transaction in self.transactions:
+                self.shipment += transaction['terms']['quantity']
+            return
+        return
 
     def make_deal(self, deal: dict) -> None:
         self.transactions.append(deal)
         return
 
-    def count_proceeds_spruce(self, market_price) -> float:
-        """Считает выручку маклера при продаже заготовок из ели"""
-        proceeds = 0
-        for shipment in self.shipments_spruce:
-            proceeds += shipment[0] * shipment[1] * shipment[2] * market_price
-        return proceeds
-
-    def count_proceeds_oak(self, market_price) -> float:
-        """Считает выручку маклера при продаже заготовок из дуба"""
-        proceeds = 0
-        for shipment in self.shipments_oak:
-            proceeds += shipment[0] * shipment[1] * shipment[2] * market_price
-        return proceeds
-
-    def count_proceeds_redwood(self, market_price) -> float:
-        """Считает выручку маклера при продаже заготовок из красного дерева"""
-        proceeds = 0
-        for shipment in self.shipments_redwood:
-            proceeds += shipment[0] * shipment[1] * shipment[2] * market_price
-        return proceeds
-
-    def count_proceeds_normal(self, market_price) -> float:
-        """Считает выручку маклера при игре на нормал"""
-        if self.shipments_normal[0][1] != 'normal':
-            raise AttributeError
-        proceeds = 0
-        for shipment in self.shipments_normal:
-            proceeds += shipment[0] * market_price
-        return proceeds
-
-    def count_purchase_costs_normal(self) -> int:
-        """Считает расходы маклера на покупку заготовок"""
-        if self.shipments_normal[0][1] != 'normal':
-            raise AttributeError
+    def count_purchase_costs(self) -> int:
+        """Считает затраты маклера на совершение сделок"""
         costs = 0
-        deals = []
         for transaction in self.transactions:
-            deals.append((transaction['terms']['billets'][0], transaction['terms']['price']))
-        for deal in deals:
-            costs += deal[0] * deal[1]
+            costs += transaction['terms']['quantity'] * transaction['terms']['price']
         return costs
 
-    def count_purchase_costs_spruce(self) -> int:
-        pass
+    def count_proceeds(self, market_price) -> float:
+        """Считает выручку от продажи заготовок"""
+        return self.shipment * market_price
 
+
+transaction_example = {
+    'producer': 0,
+    'broker': 0,
+    'terms': {
+        'quantity': 0,
+        'material': 0,
+        'machine': ('chinese', 'oak', 3),
+        'price': 0
+    }
+}
+
+
+class BrokerHard(AbstractBroker):
+    def __init__(self, balance):
+        self.balance = balance
+        self.shipments_spruce = []
+        self.shipments_oak = []
+        self.shipments_redwood = []
+        self.transactions = []
+
+    def add_shipments(self) -> None:
+        """Раскидывает заготовки на отправку согласно материалу"""
+        for transaction in self.transactions:
+            material = transaction['terms']['material']
+            if material == self.material['spruce']:
+                self.shipments_spruce.append(transaction['terms'])
+            elif material == self.material['oak']:
+                self.shipments_oak.append(transaction['terms'])
+            elif material == self.material['redwood']:
+                self.shipments_redwood.append(transaction['terms'])
+        return
+
+    def make_deal(self, deal: dict) -> None:
+        """Совершает сделку с производителем"""
+        self.transactions.append(deal)
+        return
+
+    def count_proceeds(self, market_price: dict) -> float:
+        """Считает выручку маклера от продажи заготовок"""
+        proceeds = 0
+        for shipment in self.shipments_spruce:
+            proceeds += shipment['quantity'] * shipment['material'] * shipment['machine'] * market_price['spruce']
+        for shipment in self.shipments_oak:
+            proceeds += shipment['quantity'] * shipment['material'] * shipment['machine'] * market_price['oak']
+        for shipment in self.shipments_redwood:
+            proceeds += shipment['quantity'] * shipment['material'] * shipment['machine'] * market_price['redwood']
+        return proceeds
+
+    def count_purchase_costs(self) -> int:
+        """Считает затраты маклера на покупку заготовок"""
+        costs = 0
+        spruce_purchased = [x['terms'] for x in self.transactions if x['terms']['material'] == self.material['spruce']]
+        oak_purchased = [x['terms'] for x in self.transactions if x['terms']['material'] == self.material['oak']]
+        redwood_purchased = [x['terms'] for x in self.transactions if x['terms']['material'] == self.material['redwood']]
+        for purchase in spruce_purchased:
+            costs += purchase['quantity'] * purchase['price']
+        for purchase in oak_purchased:
+            costs += purchase['quantity'] * purchase['price']
+        for purchase in redwood_purchased:
+            costs += purchase['quantity'] * purchase['price']
+        return costs
 
