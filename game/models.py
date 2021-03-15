226669de2acb9302ea7b320cf1fc956102d6cf51
BeepBoopYeetYourScoot <zdmitry@alabuga.ru>
2021-03-15 14:17:15 +0300

    Расширил модели до минимально необходимого количества

--- game/models.py
+++ game/models.py
@@ -24,8 +24,19 @@ PLAYER_NUMBER_PRESET = (
     ('31-35', '31-35 Игроков'),
 )
 
+CITIES = (
+    ('Neverfall', "Неверфол"),
+    ('Tortuga', "Тортуга"),
+    ('Wemshire', "Вемшир"),
+    ('Ivo', "Айво"),
+    ('Alendor', "Алендор"),
+    ('Etroi', "Этруа"),)
+
 
 class Session(models.Model):
+    """
+    Модель игровой сессии вместе со всеми настройками
+    """
     name = models.CharField(max_length=150)
     game_type = models.CharField(max_length=15, choices=GAME_TYPES, default='normal')
     number_of_players = models.CharField(max_length=20, choices=PLAYER_NUMBER_PRESET, default='12-14')
@@ -41,12 +52,32 @@ class Session(models.Model):
         verbose_name_plural = 'Сессии'
 
     def save(self, *args, **kwargs):
+        # Настройка начальных параметров сессии в зависимости от количества игроков
         if not self.pk:
             if self.game_type == 'normal':
                 if self.number_of_players == '12-14':
+                    if not self.number_of_brokers:
+                        self.number_of_brokers = 3
                     self.broker_starting_balance = 8000
                     self.producer_starting_balance = 4000
-                else:
+                elif self.number_of_players == "15-20":
+                    if not self.number_of_brokers:
+                        self.number_of_brokers = 4
+                    self.broker_starting_balance = 12000
+                    self.producer_starting_balance = 6000
+                elif self.number_of_players == "21-25":
+                    if not self.number_of_brokers:
+                        self.number_of_brokers = 5
+                    self.broker_starting_balance = 12000
+                    self.producer_starting_balance = 6000
+                elif self.number_of_players == "26-30":
+                    if not self.number_of_brokers:
+                        self.number_of_brokers = 6
+                    self.broker_starting_balance = 12000
+                    self.producer_starting_balance = 6000
+                elif self.number_of_players == "31-35":
+                    if not self.number_of_brokers:
+                        self.number_of_brokers = 7
                     self.broker_starting_balance = 12000
                     self.producer_starting_balance = 6000
             elif self.game_type == 'hard':
@@ -68,3 +99,32 @@ class Player(models.Model):
 
 class Producer(models.Model):
     player = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='producer')
+    city = models.CharField(max_length=20, choices=CITIES, verbose_name='Расположение')
+    balance = models.PositiveIntegerField(default=Session.producer_starting_balance)
+    billets_produced = models.PositiveIntegerField()
+    billets_stored = models.PositiveIntegerField()
+    # FIXME подключить Postgres, чтобы можно было пользоваться полем
+    #  На самом деле, даже не факт, что это поле нам понадобится
+    #  transactions = models.JSONField()
+    is_bankrupt = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'Производитель'
+        verbose_name_plural = 'Производители'
+
+    def __str__(self):
+        pass
+
+
+class Broker(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='broker')
+    city = models.CharField(max_length=20, choices=CITIES, verbose_name='Расположение')
+    balance = models.PositiveIntegerField(default=Session.broker_starting_balance)
+    is_bankrupt = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'Маклер'
+        verbose_name_plural = 'Маклеры'
+
+    def __str__(self):
+        pass
