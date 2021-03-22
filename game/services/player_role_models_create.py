fd277ea18900f7204cee8fd9cb4fbec940ad060f
LorinLex <laizybot@gmail.com>
2021-03-22 13:38:46 +0300

    Updated models create func

--- game/services/player_role_models_create.py
+++ game/services/player_role_models_create.py
@@ -2,7 +2,7 @@ from .. import models
 import random
 import copy
 
-def player_role_models_create(session, brokers, producers):
+def player_role_models_create(session_model, session_id):
 	def city_generator(players_amount, brokers_amount):
 		local_players_amount = copy.copy(players_amount)
 		cities = [
@@ -20,23 +20,25 @@ def player_role_models_create(session, brokers, producers):
 			if len(cities_for_iter) == 0:
 				cities_for_iter = cities[:brokers_amount]
 
-	city = city_generator(len(session.player.all()), len(brokers))
+	session_instance = session_model.objects.get(id=session_id)
+	players = session_instance.player.all()
+	city = city_generator(len(players), session_instance.players.filter(role=brokers).count())
 
-	producers = [
+	producer_models = [
 		models.ProducerModel.objects.create(
 			player=player,
 			session=session_instance,
 			city=next(city),
 		)
-		for player in producers
+		for player in players if player.role == 'producer'
 	]
-	brokers = [
+	broker_models = [
 		models.BrokerModel.objects.create(
 			player=player,
 			session=session_instance,
 			city=next(city),
 		)
-		for player in brokers
+		for player in players if player.role == 'brokers'
 	]
 
-	return brokers, producers
+	return broker_models, producer_models
