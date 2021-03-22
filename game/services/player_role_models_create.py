ef392f08a1efc7ba638b819aea60c8f29ddc3cab
LorinLex <laizybot@gmail.com>
2021-03-22 15:41:47 +0300

    Refactor create models func

--- game/services/player_role_models_create.py
+++ game/services/player_role_models_create.py
@@ -3,30 +3,29 @@ import random
 import copy
 
 
-def player_role_models_create(session_model, session_id):
-	def city_generator(players_amount, brokers_amount):
-		local_players_amount = copy.copy(players_amount)
-		cities = [
-			'NF',
-			'TT',
-			'WS',
-			'IV',
-			'AD',
-			'ET',
-		]
-		cities_for_iter = cities[:brokers_amount]
-		while local_players_amount > 0:
-			yield cities_for_iter.pop(random.randint(0, len(cities_for_iter) - 1))
-			local_players_amount -= 1
-			if len(cities_for_iter) == 0:
-				cities_for_iter = cities[:brokers_amount]
+def city_generator(players_amount, brokers_amount):
+	local_players_amount = copy.copy(players_amount)
+	cities = [
+		'NF',
+		'TT',
+		'WS',
+		'IV',
+		'AD',
+		'ET',
+	]
+	cities_for_iter = cities[:brokers_amount]
+	while local_players_amount > 0:
+		yield cities_for_iter.pop(random.randint(0, len(cities_for_iter) - 1))
+		local_players_amount -= 1
+		if len(cities_for_iter) == 0:
+			cities_for_iter = cities[:brokers_amount]
 
+def create_role_models(session_model, session_id):
 	session_instance = session_model.objects.get(id=session_id)
 	print(session_instance)
 	players = session_instance.player.all()
 	print(players)
 	city = city_generator(len(players), session_instance.player.filter(role='broker').count())
-	broker_models, producer_models = [], []
 
 	for player in players:
 		if player.role == 'producer':
@@ -41,7 +40,4 @@ def player_role_models_create(session_model, session_id):
 				# session=session_instance,
 				city=next(city),
 			)
-
 	del city
-
-	return broker_models, producer_models
