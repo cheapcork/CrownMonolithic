4d74a7b0ed7fd4946cdff1b14b53921bf1be3199
BeepBoopYeetYourScoot <zdmitry@alabuga.ru>
2021-03-22 13:37:40 +0300

    Рандомайзер ролей работает как надо

--- game/services/role_randomizer.py
+++ game/services/role_randomizer.py
@@ -10,23 +10,21 @@ def distribute_roles(session_model, session_id):
 	preassigned_brokers = session_instance.player.filter(role='broker')
 	preassigned_producers = session_instance.player.filter(role='producer')
 	player_models_list = list(players_queryset)
-	print(player_models_list)
+
 	for broker in preassigned_brokers:
 		player_models_list.remove(broker)
 	for producer in preassigned_producers:
 		player_models_list.remove(producer)
-	print(player_models_list)
 
 	number_of_brokers_to_distribute = session_instance.number_of_brokers - preassigned_brokers.count()
 
-	broker_players = random.sample(player_models_list, number_of_brokers_to_distribute)
-	print(broker_players)
+	broker_players_sample = random.sample(player_models_list, number_of_brokers_to_distribute)
 
 	for player in players_queryset:
-		for broker_player in broker_players:
+		for broker_player in broker_players_sample:
 			if player == broker_player:
 				player.role = 'broker'
 				player.save()
-			else:
-				player.role = 'producer'
-				player.save()
+		if not broker_players_sample:
+			player.role = 'producer'
+			player.save()
