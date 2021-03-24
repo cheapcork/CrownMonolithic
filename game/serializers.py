b5b116cc36894d32ae36b2de645dac295c407b97
LorinLex <laizybot@gmail.com>
2021-03-24 11:06:26 +0300

    Added players in session serializer

--- game/serializers.py
+++ game/serializers.py
@@ -1,32 +1,57 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import SessionModel, PlayerModel, ProducerModel, BrokerModel, TransactionModel
 
 
-class SessionSerializer(ModelSerializer):
-	class Meta:
-		model = SessionModel
-		fields = '__all__'
-
-
-class PlayerSerializer(ModelSerializer):
+class PlayerSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = PlayerModel
 		fields = '__all__'
 
 
-class ProducerSerializer(ModelSerializer):
+class SessionSerializer(serializers.ModelSerializer):
+	player = PlayerSerializer(many=True)
+	class Meta:
+		model = SessionModel
+		fields = [
+			'name',
+			'game_type',
+			'number_of_players',
+			'turn_count',
+			'number_of_brokers',
+			'crown_balance',
+			'status',
+			'broker_starting_balance',
+			'producer_starting_balance',
+			'transaction_limit',
+			'current_turn',
+			'player',
+		]
+		read_only = [
+			'game_type',
+			'number_of_brokers',
+			'player',
+		]
+
+	def get_session_players(self, instance):
+		return PlayerSerializer(
+			instance.player.all(),
+			many=True
+		)
+
+
+class ProducerSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = ProducerModel
 		fields = '__all__'
 
 
-class BrokerSerializer(ModelSerializer):
+class BrokerSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = BrokerModel
 		fields = '__all__'
 
 
-class TransactionSerializer(ModelSerializer):
+class TransactionSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = TransactionModel
 		fields = '__all__'
