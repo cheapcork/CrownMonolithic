9ba085666caa8df5dd670704f94f931ddd731b7d
LorinLex <laizybot@gmail.com>
2021-04-14 10:15:25 +0300

    Modify player presentation in admin

--- game/admin.py
+++ game/admin.py
@@ -1,8 +1,34 @@
 from django.contrib import admin
 from .models import SessionModel, PlayerModel, ProducerModel, BrokerModel, TransactionModel
+from django.utils.safestring import mark_safe
+
+
+@admin.register(PlayerModel)
+class PlayerAdmin(admin.ModelAdmin):
+    toket = admin
+    list_display = (
+        "id",
+        "nickname",
+        "get_token",
+        "session",
+        "role",
+        "city",
+        "balance",
+        "ended_turn",
+        "is_bankrupt",
+    )
+
+    list_filter = (
+        'session',
+    )
+
+    def get_token(self, obj):
+        return mark_safe(f'<span>{obj.token.key}</span>');
+
+    get_token.short_description = 'Токен'
+
 
 admin.site.register(SessionModel)
-admin.site.register(PlayerModel)
 admin.site.register(ProducerModel)
 admin.site.register(BrokerModel)
 admin.site.register(TransactionModel)
